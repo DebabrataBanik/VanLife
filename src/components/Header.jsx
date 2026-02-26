c0f0cb83e0b35386239a2cfae62d32ec6ae19265
DebabrataBanik <debabratabanik02@gmail.com>
2026-02-26 11:28:07 +0530

    feat: use NavLink for showing active nav link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router"
+import { Link, NavLink } from "react-router"
 
 export default function Header() {
   return (
     <header>
       <Link className="site-logo" to='/'>#VANLIFE</Link>
       <nav>
-        <Link to='/host'>Host</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/vans'>Vans</Link>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/host'
+        >
+          Host
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/about'
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/vans'
+        >
+          Vans
+        </NavLink>
       </nav>
     </header>
   )
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,14 +1,17 @@
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 export default function HostLayout() {
   return (
-    <>
+    <div className="host-layout">
       <nav>
-        <Link to='/host'>Dashboard</Link>
-        <Link to='/host/income'>Income</Link>
-        <Link to='/host/reviews'>Reviews</Link>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/host' end >Dashboard</NavLink>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/host/income'>Income</NavLink>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active' : null} to='/host/reviews'>Reviews</NavLink>
       </nav>
       <Outlet />
-    </>
+    </div>
   )
 }
